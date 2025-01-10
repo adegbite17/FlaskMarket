79c07216907994423df6eaaee43dc5024fcdfbd6
adegbite17 <boye17@icloud.com>
2025-01-10 20:00:22 +0100

    Backend code for flask market

---
+++ FlaskMarket/market/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+database_path = r'C:\Users\USER\sqlite\market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
+app.config['SECRET_KEY'] = 'aaa84d2a6a9b0215c9113cf5'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from . import routes
---
+++ FlaskMarket/run.py
@@ -0,0 +1,7 @@
+from market import app
+
+app.config['DEBUG'] = True  # Enable debug mode
+
+# Checks if the run.py file has executed directly and not imported
+if __name__ == '__main__':
+    app.run(debug=True)
