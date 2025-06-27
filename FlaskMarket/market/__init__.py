08df5bc167d69a08e0de6587e82b173024af8347
Azeez Adegbite <107010791+adegbite17@users.noreply.github.com>
2025-06-27 23:13:12 +0000

    update

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-database_path = r'C:\Users\USER\sqlite\market.db'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+database_path = os.path.join(BASE_DIR, 'market.db')
+#database_path = r'C:\Users\USER\sqlite\market.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
 app.config['SECRET_KEY'] = 'aaa84d2a6a9b0215c9113cf5'
 db = SQLAlchemy(app)
